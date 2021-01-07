d81e19457a1fbd3ec32a4062adc9937fd5f248e8
paveltom <paveltom@post.bgu.ac.il>
2021-01-07 09:57:15 +0200

    bin folder added makefile fixed

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,32 @@
 #include "../include/ConnectionHandler.h"
 #include "../include/Task.h"
-#include "../include/EncoderDecoder.h"
-
-#include <stdexcept>
 #include <iostream>
-#include <thread>
-#include <mutex>
 
-using namespace std;
+int main(int argc, char** argv) {
 
-/**
-* This code assumes that the server replies the exact text the client sent it (as opposed to the practical session example)
-*/
-int main (int argc, char *argv[]) {
+//    if (argc < 3) {
+//        cerr << "Usage: " << argv[0] << " host port" << endl << endl;
+//        return -1;
+//    }
 
-    if (argc < 3) {
-        cerr << "Usage: " << argv[0] << " host port" << endl << endl;
-        return -1;
-    }
-
-    string host = argv[1];
-    short port = atoi(argv[2]);
+//    string host = argv[1];
+//    short port = atoi(argv[2]);
+    std::string host = "127.0.0.1";
+    short port = 7777;
     
     ConnectionHandler connectionHandler(host, port);
     if (!connectionHandler.connect()) {
-        cerr << "Cannot connect to " << host << ":" << port << endl;
+        std::cerr << "Cannot connect to " << host << ":" << port << std::endl;
         return 1;
     }
 
-    mutex mutex;//probably not needed
+    std::mutex mutex;//probably not needed
 
     KeyboardTask keyboardTask(connectionHandler, 1, mutex);
     NetTask netTask(connectionHandler, 2, mutex);
 
-    thread th1(&Task::run, &keyboardTask);
-    thread th2(&Task::run, &netTask);
+    std::thread th1(&Task::run, &keyboardTask);
+    std::thread th2(&Task::run, &netTask);
 
     th1.join(); // or detach()?
     th2.join(); //or detach()?
