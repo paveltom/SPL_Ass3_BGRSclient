c726d30817b5cde43c8f4541402d08cf10f3c79a
paveltom <paveltom@post.bgu.ac.il>
2021-01-08 01:07:03 +0200

    took hila's versions for ED.cpp:decode() and CH.cpp:getline()

--- include/EncoderDecoder.h
+++ include/EncoderDecoder.h
@@ -9,11 +9,17 @@ using boost::asio::ip::tcp;
 using namespace std;
 
 class EncoderDecoder {
+private:
+    int len;
+    char decodeBytes[1<<10];
+    vector<char>* vecBytes = new vector<char>;
+    short opCode = 0;
+    string result = "";
 public:
     EncoderDecoder();
 
-    char* encode(const std::string& msg, string& size); //returns bytes[]
-//    const string decode( char bytes[]); // why const char in argument??
+    char* encode(const std::string& msg, string& size, char* output); //returns bytes[]
+    const string decode( char c); // why const char in argument??
     short bytesToShort(char* bytesArr);
     void shortToBytes(short num, char* bytesArr);
 
